83a67cef3bed46010e1fc1294ee2b0a5cba799d0
Mansi Gupta <mansi.gupta@codesquaretech.com>
2022-07-11 18:12:21 +0530

    details Page- Mansi

---
+++ detailsPage.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+        $('#header').load('header.html');
+        $('#footer').load('footer.html');
+        var category = 'Clothing';
+        var products = 'Men';
+        var productName = 'T-shirts';
+         $('#productRoute').text('hello');
+         
+})
+ // var category = $('#category').html;
+    // var products = $('#products').html;
+    // var productName = $('#productName').html;
+    
+
+    
+
+   
