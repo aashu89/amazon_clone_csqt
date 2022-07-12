875bb134c377d57bb3b6082e260676682969d233
Mansi Gupta <mansi.gupta@codesquaretech.com>
2022-07-12 15:37:08 +0530

    edit details Page- Mansi

--- detailsPage.js
+++ detailsPage.js
@@ -1,15 +1,21 @@
 $(document).ready(function(){
-        $('#header').load('header.html');
+        $('#header').load('topHEader.html');
         $('#footer').load('footer.html');
+        // var category = $('#category').html;
+    // var products = $('#products').html;
+    // var productName = $('#productName').html;
         var category = 'Clothing';
         var products = 'Men';
         var productName = 'T-shirts';
-         $('#productRoute').text('hello');
-         
+
+        // $('#productRoute').text(`${category} > ${products} > ${productName}`);
+        $('#productRoute').text("Clothing > Men > T-shirts");
+        $('#productName').text("Amazon Brand - Myx Women's Cotton Kurti");
+        $('#price').html(`&#8377 299.00 - &#8377 1,007.00`);
+        // $('#rating').mdbRate();
+       
 })
- // var category = $('#category').html;
-    // var products = $('#products').html;
-    // var productName = $('#productName').html;
+
     
 
     
